5bc272c8cda4ed5fd7864c3c51555865bfa301cc
RishirajSrkr <113599778+RishirajSrkr@users.noreply.github.com>
2025-01-06 22:43:50 +0530

    ui

--- src/components/GoogleCallback.jsx
+++ src/components/GoogleCallback.jsx
@@ -2,7 +2,7 @@ import { useEffect, useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from '../config/axiosConfig';
 import { AuthContext } from '../context/AuthContext';
-import jwtDecode from 'jwt-decode'; // Correct import for jwtDecode
+import {jwtDecode} from 'jwt-decode'; // Correct import for jwtDecode
 
 function GoogleCallback() {
     const { setLoggedInUser } = useContext(AuthContext);
